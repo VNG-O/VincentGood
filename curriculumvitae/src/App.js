367d42f06da7fb6d4d2441534db20c4795d14863
vngood <vincentolivier100@gmail.com>
2021-07-20 13:17:39 +0200

    First Commit

---
+++ curriculumvitae/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div>
+      {/* testing */}
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
